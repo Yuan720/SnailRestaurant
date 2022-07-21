213b2e8cab8adfbda5014574325fa040f35111f3
Yuan <mhtlyy@gmail.com>
2022-07-22 00:38:11 +0800

    配置swagger扫描包

--- src/main/java/com/woniuxy/snailrestaurant/config/SwaggerConfig.java
+++ src/main/java/com/woniuxy/snailrestaurant/config/SwaggerConfig.java
@@ -2,10 +2,10 @@ package com.woniuxy.snailrestaurant.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.oas.annotations.EnableOpenApi;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
-import springfox.documentation.service.VendorExtension;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 
@@ -17,7 +17,10 @@ public class SwaggerConfig {
     @Bean
     public Docket docket() {
         return new Docket(DocumentationType.SWAGGER_2)
-                .apiInfo(apiInfo());//配置Swagger信息
+                //配置Swagger信息
+                .apiInfo(apiInfo()).select()
+                .apis(RequestHandlerSelectors
+                        .basePackage("com.woniuxy.snailrestaurant.controller")).build();
     }
 
     //配置Swagger信息
@@ -32,7 +35,7 @@ public class SwaggerConfig {
                 "智慧餐厅Swagger API文档",
                 "V1.0",
                 "https://google.com",
-                new Contact("李小亮,孟汶", "https://google.com", "dev@google.com"),
+                new Contact("yuan", "https://google.com", "dev@google.com"),
                 /*Contact(String name, String url, String email)*/
                 "Apache 2.0",
                 "http://www.apache.org/licenses/LICENSE-2.0",
