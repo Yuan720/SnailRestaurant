d5a8f104a2706e908e51deda21d5c485a83f5747
Yuan <mhtlyy@gmail.com>
2022-07-21 14:54:36 +0800

    Initial commit

---
+++ src/main/java/com/woniuxy/snailrestaurant/SnailRestaurantApplication.java
@@ -0,0 +1,13 @@
+package com.woniuxy.snailrestaurant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnailRestaurantApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SnailRestaurantApplication.class, args);
+    }
+
+}
