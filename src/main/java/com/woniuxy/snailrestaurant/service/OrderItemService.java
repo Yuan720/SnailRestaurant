8229ba35ddda8fa4f8be3bc583f7f0381a29afbc
laomeng <839235074@qq.com>
2022-07-27 15:36:56 +0800

    实现查询订单菜品列表接口

--- src/main/java/com/woniuxy/snailrestaurant/service/OrderItemService.java
+++ src/main/java/com/woniuxy/snailrestaurant/service/OrderItemService.java
@@ -1,7 +1,12 @@
 package com.woniuxy.snailrestaurant.service;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.woniuxy.snailrestaurant.domain.OrderItem;
 import com.baomidou.mybatisplus.extension.service.IService;
+import io.swagger.annotations.ApiParam;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
 
 /**
 * @author LeeYuan
@@ -9,5 +14,6 @@ import com.baomidou.mybatisplus.extension.service.IService;
 * @createDate 2022-07-25 20:27:40
 */
 public interface OrderItemService extends IService<OrderItem> {
+    List<OrderItem> findByOrderNumer(String orderNumber);
 
 }
