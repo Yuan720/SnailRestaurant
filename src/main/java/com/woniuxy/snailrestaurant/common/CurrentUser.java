da2aaf41847cf38f19b81de97e1086103631d472
Yuan <mhtlyy@gmail.com>
2022-07-28 09:20:54 +0800

    实现认证信息注入

---
+++ src/main/java/com/woniuxy/snailrestaurant/common/CurrentUser.java
@@ -0,0 +1,13 @@
+package com.woniuxy.snailrestaurant.common;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * 在Controller的方法参数使用此注解，该方法在映射时会注入当前登录的User对象
+ */
+@Target(ElementType.PARAMETER)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface CurrentUser {
+}
