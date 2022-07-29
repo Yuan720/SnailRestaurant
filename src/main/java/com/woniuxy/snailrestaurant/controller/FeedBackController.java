61ea1ec622a03a5f26fb0ba70f4016df0cdd4b9b
Yuan <mhtlyy@gmail.com>
2022-07-29 17:04:38 +0800

    添加反馈

--- src/main/java/com/woniuxy/snailrestaurant/controller/FeedBackController.java
+++ src/main/java/com/woniuxy/snailrestaurant/controller/FeedBackController.java
@@ -1,24 +1,44 @@
 package com.woniuxy.snailrestaurant.controller;
 
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.woniuxy.snailrestaurant.common.CurrentUser;
 import com.woniuxy.snailrestaurant.common.CurrentUserInfo;
 import com.woniuxy.snailrestaurant.domain.Feedback;
+import com.woniuxy.snailrestaurant.domain.FeedbackType;
+import com.woniuxy.snailrestaurant.service.FeedbackService;
+import com.woniuxy.snailrestaurant.service.FeedbackTypeService;
 import io.swagger.annotations.Api;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import io.swagger.annotations.ApiOperation;
+import org.apache.ibatis.annotations.Param;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Api(tags = "反馈")
 @RestController
 @RequestMapping("/feedback")
 public class FeedBackController {
+    @Autowired
+    FeedbackTypeService typeService;
+    @Autowired
+    FeedbackService service;
 
+    @ApiOperation("提交反馈")
     @PostMapping
-    int addFeedBack(@CurrentUser CurrentUserInfo info,
-                    @RequestBody Feedback feedBack) {
+    boolean addFeedBack(@CurrentUser CurrentUserInfo info,
+                        @RequestBody Feedback feedBack) {
 
-        return 0;
+        feedBack.setUserId(info.getId());
+        boolean save = service.save(feedBack);
+        return save;
+    }
+
+    @ApiOperation(value = "列出反馈类型")
+    @GetMapping("/types")
+    List<FeedbackType> listAll() {
+        List<FeedbackType> feedbackTypes = typeService.getBaseMapper().selectList(null);
+        return feedbackTypes;
     }
 
 }
