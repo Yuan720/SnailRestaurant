c3fe28b3873a137e1fc47c2595333bac62b3c29b
Yuan <mhtlyy@gmail.com>
2022-07-29 14:55:32 +0800

    评论添加接口实现

--- src/main/java/com/woniuxy/snailrestaurant/controller/DeliverInfoController.java
+++ src/main/java/com/woniuxy/snailrestaurant/controller/DeliverInfoController.java
@@ -6,43 +6,38 @@ import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.woniuxy.snailrestaurant.common.CurrentUser;
 import com.woniuxy.snailrestaurant.common.CurrentUserInfo;
 import com.woniuxy.snailrestaurant.domain.DeliverInfo;
-import com.woniuxy.snailrestaurant.domain.Dishes;
 import com.woniuxy.snailrestaurant.service.DeliverInfoService;
 import io.swagger.annotations.*;
 import org.springframework.web.bind.annotation.*;
-
 import javax.annotation.Resource;
-import java.net.Inet4Address;
-import java.net.InetAddress;
-import java.net.UnknownHostException;
-import java.util.List;
+
 
 @Api(tags = "地址信息")
 @RestController("/deliverInfo")
 public class DeliverInfoController {
     @Resource
     private DeliverInfoService dis;
+
     @ApiOperation(value = "获取用户收餐信息表")
     @GetMapping
     @ApiImplicitParams({
             @ApiImplicitParam(name = "id", value = "用户id", required = true),
-            @ApiImplicitParam(name = "offset", value = "分页起始值", required = false),
-            @ApiImplicitParam(name = "pageSize", value = "分页大小", required = false)
+            @ApiImplicitParam(name = "offset", value = "分页起始值"),
+            @ApiImplicitParam(name = "pageSize", value = "分页大小")
     })
-    IPage getDeliverInfoByUserId(
+    IPage<DeliverInfo> getDeliverInfoByUserId(
             @RequestParam(value = "offset", defaultValue = "0", required = false) int offset,
             @RequestParam(value = "pageSize", defaultValue = "10", required = false) int pageSize,
             @CurrentUser CurrentUserInfo info) {
-        IPage page = new Page(offset,pageSize);
+        IPage<DeliverInfo> page = new Page(offset, pageSize);
         LambdaQueryWrapper<DeliverInfo> lqw = new LambdaQueryWrapper<DeliverInfo>();
-        lqw.eq(DeliverInfo::getUserId,info.getId());
-        IPage iPage = dis.page(page,lqw);
-        return iPage;
+        lqw.eq(DeliverInfo::getUserId, info.getId());
+        return dis.page(page, lqw);
     }
 
     @ApiOperation(value = "增加用户收餐信息")
     @PostMapping
-    int addDeliverInfo( @ApiParam(name = "id", value = "用户id") @CurrentUser CurrentUserInfo info
+    int addDeliverInfo(@ApiParam(name = "id", value = "用户id") @CurrentUser CurrentUserInfo info
             , @RequestBody @ApiParam(name = "deliverInfo", value = "收餐信息json") DeliverInfo deliverInfo) {
         return 0;
     }
