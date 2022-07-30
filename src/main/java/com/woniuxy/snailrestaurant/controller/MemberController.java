c1ebba1321a256d54313817b1c57fb64285ef0dd
userhuang <2926378568.@qq.com>
2022-07-30 21:44:01 +0800

    添加根据ID删除会员信息的功能

--- src/main/java/com/woniuxy/snailrestaurant/controller/MemberController.java
+++ src/main/java/com/woniuxy/snailrestaurant/controller/MemberController.java
@@ -1,6 +1,7 @@
 package com.woniuxy.snailrestaurant.controller;
 
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.woniuxy.snailrestaurant.common.CommonResultCode;
 import com.woniuxy.snailrestaurant.domain.qo.MemberCardQo;
 import com.woniuxy.snailrestaurant.domain.vo.MemberDetailVo;
 import com.woniuxy.snailrestaurant.service.MemberCardService;
@@ -8,10 +9,7 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiImplicitParams;
 import io.swagger.annotations.ApiOperation;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 
@@ -23,19 +21,32 @@ public class MemberController {
     @Resource
     private MemberCardService memberCardService;
 
-   @ApiOperation("列出会员信息")
-   @ApiImplicitParams({
-           @ApiImplicitParam(name = "MemberCardQo", value = "会员查询条件",paramType = "query"),
-           @ApiImplicitParam(name = "current", value = "起始页",paramType = "query"),
-           @ApiImplicitParam(name = "size", value = "页大小",paramType = "query")
-   })
+    @ApiOperation("列出会员信息")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "MemberCardQo", value = "会员查询条件", paramType = "query"),
+            @ApiImplicitParam(name = "current", value = "起始页", paramType = "query"),
+            @ApiImplicitParam(name = "size", value = "页大小", paramType = "query")
+    })
 
     @GetMapping("/list")
     public Page<MemberDetailVo> listMember(MemberCardQo memberCardQo,
-                                           @RequestParam(value = "page",defaultValue = "1")  Integer current,
-                                           @RequestParam(value = "size",defaultValue = "10")   Integer size) {
+                                           Integer current,
+                                           Integer size) {
 
-        return memberCardService.listByCond(memberCardQo,current,size);
+        return memberCardService.listByCond(memberCardQo, current, size);
+    }
 
+    @DeleteMapping("/{id}")
+    @ApiOperation(value = "删除会员信息")
+    @ApiImplicitParams({
+            //query 表示参数来自于  查询参数
+            @ApiImplicitParam(name = "id", value = "会员id", paramType = "path")
+    })
+    Boolean deleteMember(@PathVariable("id") Integer id) {
+
+
+        boolean b = memberCardService.removeById(id);
+
+        return b;
     }
 }
